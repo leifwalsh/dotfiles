4d12cbcb173c34faf513036d6af73b382e3574c9
Leif Walsh <leif.walsh@gmail.com>
2009-06-06 13:37:13 -0700

    adding something to emacs for C

--- .emacs.real.el
+++ .emacs.real.el
@@ -12,23 +12,24 @@
                 ((symbolp e) (require e)))) args)
       (file-error  (progn (message "Couldn't load extension: %s" lib) nil)))))
 
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 77 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(c-echo-syntactic-information-p t)
  '(column-number-mode t)
  '(display-battery-mode t)
  '(display-time-mode t)
  '(inhibit-startup-screen t)
  '(scroll-bar-mode nil)
  '(show-paren-mode t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 77 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
 
 ;; start leif's config
 (setq user-mail-address "leif.walsh@gmail.com")
