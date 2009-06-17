9c32bc004ce7bded37ea1140915ee6fb8b60a668
Leif Walsh <leif.walsh@gmail.com>
2009-06-17 01:57:33 -0700

    Added transparency toggle.

--- .emacs.d/set-color.el
+++ .emacs.d/set-color.el
@@ -2,4 +2,15 @@
 (setq color-theme-is-global t)
 (require 'color-theme-sunburst)
 (color-theme-tm)
+
+(defun toggle-transparency ()
+  (interactive)
+  (if (/=
+       (cadr (find 'alpha (frame-parameters nil) :key #'car))
+       100)
+      (set-frame-parameter nil 'alpha '(100 100))
+    (set-frame-parameter nil 'alpha '(80 50))))
+(set-frame-parameter nil 'alpha '(80 50))
+(global-set-key (kbd "C-c C-t") 'toggle-transparency)
+
 (provide 'set-color)
