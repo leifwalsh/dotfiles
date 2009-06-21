86bb0d139bbf5ea1076d96957b7dd0566b05d3c0
Leif Walsh <leif.walsh@gmail.com>
2009-06-20 21:21:28 -0700

    Bringing font size down to 72 so I can get two 80-char windows side-by-side in fullscreen.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -44,4 +44,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:slant normal :weight normal :height 76 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
+ '(default ((t (:slant normal :weight normal :height 72 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
