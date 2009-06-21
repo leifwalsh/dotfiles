8033fee8aaece2d7a2245542c0478107fdb9d8f4
Leif Walsh <leif.walsh@gmail.com>
2009-06-21 14:34:40 -0700

    Adding c-style to safe local variable values.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -34,6 +34,7 @@
  '(inhibit-startup-message t)
  '(inhibit-startup-screen t)
  '(line-number-mode t)
+ '(safe-local-variable-values (quote ((c-indentation-style . linux))))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(show-trailing-whitespace t)
@@ -44,4 +45,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:slant normal :weight normal :height 72 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
+ '(default ((t (:slant normal :weight normal :height 96 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
