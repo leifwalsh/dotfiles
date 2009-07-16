3b4e80e0be6289a69e1e9e655433de3835a9d908
Leif Walsh <leif.walsh@gmail.com>
2009-07-15 20:58:19 -0700

    Turning on whitespace-mode for all buffers.

--- .emacs.d/set-color.el
+++ .emacs.d/set-color.el
@@ -13,4 +13,9 @@
 (set-frame-parameter nil 'alpha '(85 60))
 (global-set-key (kbd "C-c C-t") 'toggle-transparency)
 
+;; whitespace-mode helps you have shorter lines!  Yay!
+(setq whitespace-style '(lines-tail empty trailing))
+(defun turn-on-whitespace-mode () (whitespace-mode 1))
+(add-hook 'after-change-major-mode-hook 'turn-on-whitespace-mode)
+
 (provide 'set-color)
