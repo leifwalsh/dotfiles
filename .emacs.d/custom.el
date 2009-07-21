ac5a087ca3d225d8cb0a88a39dbfb1a99be23e13
Leif Walsh <leif.walsh@gmail.com>
2009-07-20 21:08:42 -0700

    Adding nick face for erc.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -40,4 +40,5 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:slant normal :weight normal :height 96 :width normal :foundry "unknown" :family "Lucida Sans Typewriter")))))
+ '(default ((t (:slant normal :weight normal :height 96 :width normal :foundry "unknown" :family "Lucida Sans Typewriter"))))
+ '(erc-my-nick-face ((t (:foreground "cyan" :weight bold)))))
