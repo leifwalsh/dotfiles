94d042919f5ce1523ddb762880964db2cdaf5535
Leif Walsh <leif.walsh@gmail.com>
2009-09-28 11:08:09 +0200

    fixed some battery/AC detection stuff (still need to calculate time until charged, dunno where to find that info)

--- bin/parsebat.sh
+++ bin/parsebat.sh
@@ -1,35 +1,42 @@
 #!/bin/zsh
 
 for ((;;)) {
-    local E_f=$(</sys/class/power_supply/BAT0/energy_full)
-    local E_n=$(</sys/class/power_supply/BAT0/energy_now)
-    local C_n=$(</sys/class/power_supply/BAT0/current_now)
+    if [[ ( -d /sys/class/power_supply/BAT0 ) &&
+                ( $(</sys/class/power_supply/BAT0/present) -eq 1 ) ]]; then
+        local E_f=$(</sys/class/power_supply/BAT0/energy_full)
+        local E_n=$(</sys/class/power_supply/BAT0/energy_now)
+        (( PCT = 100. * E_n / E_f ))
 
-    (( PCT = 100. * E_n / E_f ))
-    (( H = E_n / C_n ))
-    (( M = ((1. * E_n / C_n) - H) * 60 ))
+        print -n "{+b .B}["
 
-    print -n "{+b .B}["
+        if [[ $PCT -gt 50.0 ]]; then
+            print -n "{= .G}"
+        elif [[ $PCT -gt 25.0 ]]; then
+            print -n "{= .Y}"
+        else
+            print -n "{= .R}"
+        fi
 
-    if [[ $PCT -gt 50.0 ]]; then
-        print -n "{= .G}"
-    elif [[ $PCT -gt 25.0 ]]; then
-        print -n "{= .Y}"
-    else
-        print -n "{= .R}"
+        print -f "%.02f%%{-} " $PCT
+
+        if [[ $(</sys/class/power_supply/AC/online) -eq 0 ]]; then
+            local C_n=$(</sys/class/power_supply/BAT0/current_now)
+
+            (( H = E_n / C_n ))
+            (( M = ((1. * E_n / C_n) - H) * 60 ))
+
+            if [[ $H -ge 1 ]]; then
+                print -n "{= .G}"
+            elif [[ $M -ge 30 ]]; then
+                print -n "{= .Y}"
+            else
+                print -n "{= .R}"
+            fi
+
+            print -f "%d:%02d{-}" $H $M
+        fi
+
+        print "]{-}"
     fi
-
-    print -f "%.02f%%{-} " $PCT
-
-    if [[ $H -ge 1 ]]; then
-        print -n "{= .G}"
-    elif [[ $M -ge 30 ]]; then
-        print -n "{= .Y}"
-    else
-        print -n "{= .R}"
-    fi
-
-    print -f "%d:%02d{-}]{-}\n" $H $M
-
     sleep 5
 }
