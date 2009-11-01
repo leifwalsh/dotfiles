dca7a89ed602e5c3f7d0b04218aa5cd196f89285
Leif Walsh <leif.walsh@gmail.com>
2009-11-01 20:46:20 +0100

    making flymake automatically load

--- .emacs.d/flymake-config.el
+++ .emacs.d/flymake-config.el
@@ -1,3 +1,4 @@
+(add-hook 'find-file-hook 'flymake-find-file-hook)
 (setq flymake-gui-warnings-enabled nil)
 (global-set-key "\C-cn" 'flymake-goto-next-error)
 (global-set-key "\C-cp" 'flymake-goto-prev-error)
