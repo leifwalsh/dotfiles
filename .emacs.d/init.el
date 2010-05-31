fc4ce7199a8bf72ce525905a5da6f2c80a389fa7
Leif Walsh <leif.walsh@gmail.com>
2010-05-31 10:39:53 +0200

    beta paredit, adding parenface I didn't have before

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -446,7 +446,10 @@
 
 ;;{{{ paredit
 
-;; already autoloaded by elpa
+(autoload 'paredit-mode "paredit"
+  "Minor mode for pseudo-structurally editing Lisp code."
+  t)
+
 (defun lisp-enable-paredit-hook () (paredit-mode 1))
 (add-hook 'clojure-mode-hook 'lisp-enable-paredit-hook)
 (add-hook 'scheme-mode-hook 'lisp-enable-paredit-hook)
@@ -459,8 +462,36 @@
 ;;{{{ clojure-mode
 
 ;; already autoloaded by elpa
-(add-hook 'clojure-mode-hook (paren-face-add-support
-                              clojure-font-lock-keywords))
+;(add-hook 'clojure-mode-hook (paren-face-add-support
+;                              clojure-font-lock-keywords))
+
+(defmacro defclojureface (name color desc &optional others)
+  `(defface ,name
+     '((((class color)) (:foreground ,color ,@others))) ,desc :group 'faces))
+
+(defclojureface clojure-parens       "DimGrey"   "Clojure parens")
+(defclojureface clojure-braces       "#49b2c7"   "Clojure braces")
+(defclojureface clojure-brackets     "SteelBlue" "Clojure brackets")
+(defclojureface clojure-keyword      "khaki"     "Clojure keywords")
+(defclojureface clojure-namespace    "#c476f1"   "Clojure namespace")
+(defclojureface clojure-java-call    "#4bcf68"   "Clojure Java calls")
+(defclojureface clojure-special      "#b8bb00"   "Clojure special")
+(defclojureface clojure-double-quote "#b8bb00"   "Clojure special"
+  (:background "unspecified"))
+
+(defun tweak-clojure-syntax ()
+  (mapcar (lambda (x) (font-lock-add-keywords nil x))
+          '((("#?['`]*(\\|)"                 . 'clojure-parens))
+            (("#?\\^?{\\|}"                  . 'clojure-brackets))
+            (("\\[\\|\\]"                    . 'clojure-braces))
+            ((":\\w+"                        . 'clojure-keyword))
+            (("#?\""               0 'clojure-double-quote prepend))
+            (("nil\\|true\\|false\\|%[1-9]?" . 'clojure-special))
+            (("(\\(\\.[^ \n)]*\\|[^ \n)]+\\.\\|new\\)\\([ )\n]\\|$\\)"
+              1 'clojure-java-call))
+            )))
+
+(add-hook 'clojure-mode-hook 'tweak-clojure-syntax)
 
 ;;}}}
 
