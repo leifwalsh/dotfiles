7744b1acf142044414b0b6a89c09babb25121293
Leif Walsh <leif.walsh@gmail.com>
2011-01-05 16:18:51 -0500

    c-eldoc

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -323,12 +323,15 @@
 
 ;;{{{ eldoc
 
+(require 'eldoc)
+
 (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'scheme-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
 
 ;;}}}
 
