cb54ca02644176d030401830bf6e5bee4a64f259
Leif Walsh <leif.walsh@gmail.com>
2011-01-06 16:58:16 -0500

    fix an eval-after-load

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -266,16 +266,17 @@
             (flymake-mode 1)))
 
 (eval-after-load "flymake"
-  (defun flymake-pylint-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "epylint" (list local-file))))
-  
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pylint-init)))
+  '(progn
+     (defun flymake-pylint-init ()
+       (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                          'flymake-create-temp-inplace))
+              (local-file (file-relative-name
+                           temp-file
+                           (file-name-directory buffer-file-name))))
+         (list "epylint" (list local-file))))
+
+     (add-to-list 'flymake-allowed-file-name-masks
+                  '("\\.py\\'" flymake-pylint-init))))
 
 ;;}}}
 
