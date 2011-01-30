081b8f3364822a48621ca1457ce73cc465919711
Leif Walsh <leif.walsh@gmail.com>
2011-01-30 14:37:21 -0500

    saving some configs before a reinstall

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -669,14 +669,13 @@
  '(tool-bar-mode nil)
  '(tooltip-mode nil)
  '(tramp-default-method "ssh")
- '(transient-mark-mode t)
  '(user-mail-address "leif.walsh@gmail.com"))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "Cousine"))))
+ '(default ((t (:inherit nil :stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
  '(erc-input-face ((t (:foreground "cyan"))))
  '(erc-my-nick-face ((t (:foreground "cyan" :weight bold)))))
 
--- .profile
+++ .profile
@@ -10,7 +10,7 @@ if [ -f ${HOME}/.termcap ]; then
 fi
 
 export CLOJURE_EXT=$HOME/.clojure:$HOME/.clojure/ext
-export PATH=$PATH:$HOME/bin:$HOME/.cabal/bin:$HOME/.gem/ruby/1.8/bin:/var/lib/gems/1.8/bin:$HOME/git/clojure-contrib/launchers/bash
+export PATH=$HOME/.cabal/bin:$PATH:$HOME/bin:$HOME/.gem/ruby/1.8/bin:/var/lib/gems/1.8/bin:$HOME/git/clojure-contrib/launchers/bash
 alias clj=clj-env-dir
 export MANPATH=:$HOME/svn/scala-2.8.1_RC1/build/scaladoc/manual/man
 
