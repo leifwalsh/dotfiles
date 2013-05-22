4804da67bf43d8ae6c1411d3b8cc692562bc73aa
Leif Walsh <leif.walsh@gmail.com>
2013-05-22 14:15:03 -0400

    add ack-and-a-half

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -631,6 +631,24 @@ header"
 
 ;;}}}
 
+;;{{{ ack/ag
+
+(when (not (package-installed-p 'ack-and-a-half))
+  (package-install 'ack-and-a-half))
+
+(when-let (ag (executable-find "ag"))
+  (setq ack-and-a-half-executable ag))
+
+(setq ack-and-a-half-prompt-for-directory t)
+
+(defadvice ack-and-a-half-arguments-from-options (after
+                                                  compatible-ag-options
+                                                  activate)
+  (setq ad-return-value (delete "--env" ad-return-value))
+  (setq ad-return-value (delete "--noenv" ad-return-value)))
+
+;;}}}
+
 ;;{{{ compile
 
 (require 'compile)
