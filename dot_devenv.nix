d23e5fb835ec8e24d3dbfe9b0bfb0f4fff92f7e5
Leif Walsh <leif.walsh@gmail.com>
2022-07-11 01:21:37 -0400

    add nvim

--- dot_devenv.nix
+++ dot_devenv.nix
@@ -1,1 +1,1 @@
-with import <nixpkgs>{}; [ bat bottom delta direnv du-dust fd fzf gh lsd procs ripgrep sd tealdeer ]
+with import <nixpkgs>{}; [ bat bottom delta direnv du-dust fd fzf gh lsd nvim procs ripgrep sd tealdeer ]
